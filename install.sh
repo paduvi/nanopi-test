66f0df09b272493c36988d7b777864c4846849ad
Phan Đức Việt <viet@techmaster.vn>
2017-06-25 18:03:01 +0700

    update install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Electron's version.
+export npm_config_target=1.6.10
+# The architecture of Electron, can be ia32 or x64.
+export npm_config_arch=arm
+export npm_config_target_arch=arm
+# Download headers for Electron.
+export npm_config_disturl=https://atom.io/download/electron
+# Tell node-pre-gyp that we are building for Electron.
+export npm_config_runtime=electron
+# Tell node-pre-gyp to build module from source code.
+export npm_config_build_from_source=true
+# Install all dependencies, and store cache to ~/.electron-gyp.
+HOME=~/.electron-gyp npm install
